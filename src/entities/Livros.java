397690ecb9d14393bddefd622cc3fc8e58c5b67d
Fabin-27 <fabiolourenco063@gmail.com>
2024-10-28 07:42:45 -0300

    classe livros criada

--- src/entities/Livros.java
+++ src/entities/Livros.java
@@ -1,5 +1,48 @@
 package entities;
 
 public class Livros {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int anoPublicacao;
+
+    public Livros(String titulo, String autor, String isbn, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
 
 }
